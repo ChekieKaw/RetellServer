7aa595c8e328ae3180972715364054a5e477381d
ChekieKaw <guojw08@163.com>
2017-04-28 15:12:12 +0800

    V1.0.0
    设置了两个TCP服务器并调用了接收类和发送类。端口4123/4124.
    当任意一个端口接收数据后马上转发给另外一个端口。从而实现服务器数据转发功能。

---
+++ reteller/src/main/java/edu/sysu/reteller/HearServiceHandler.java
@@ -0,0 +1,66 @@
+package edu.sysu.reteller;
+
+/**
+ * Created by sunny on 17-4-26.
+ */
+// EchoCommandHandler.java
+
+import org.quickserver.net.server.*;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+
+
+
+public class HearServiceHandler implements ClientEventHandler,ClientCommandHandler,ClientBinaryHandler {
+
+    public static ClientHandler hearHandler;
+
+    public void sendBinary(byte[] data){
+        try{
+            hearHandler.sendClientBinary(data);
+        }catch(IOException e){
+            System.out.println("Error relaying data!");
+        }
+
+    }
+
+    public void gotConnected(ClientHandler handler) throws  IOException {
+        hearHandler=handler;
+        handler.setDataMode(DataMode.BINARY, DataType.IN);
+        handler.setDataMode(DataMode.BINARY,DataType.OUT);
+
+    }
+
+    public void lostConnection(ClientHandler handler)
+
+            throws IOException {
+
+        handler.sendSystemMsg("Connection lost : " +
+
+                handler.getSocket().getInetAddress());
+
+    }
+
+    public void closingConnection(ClientHandler handler)
+
+            throws IOException {
+
+        handler.sendSystemMsg("Closing connection : " +
+
+                handler.getSocket().getInetAddress());
+
+    }
+
+
+    public void handleCommand(ClientHandler handler, String command){
+        //no need to implement it
+    }
+    public void handleBinary(ClientHandler handler, byte[] data) throws SocketTimeoutException, IOException {
+
+        //BASE64Encoder mEncoder= new BASE64Encoder();
+        RetellServiceHandler.retellHandler.sendClientBinary(data);
+
+    }
+
+}
